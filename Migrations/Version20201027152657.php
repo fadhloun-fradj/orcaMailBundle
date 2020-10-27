6d5d3dbeb1a3654abf19edac96a93e09e456973a
Anasel23 <anasokok@gmail.com>
2020-10-27 16:28:24 +0100

    Ajout Migrations unique

--- Migrations/Version20201027152657.php
+++ Migrations/Version20201027152657.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace DoctrineMigrations;
+namespace OrcaMailBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -10,21 +10,15 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200918143923 extends AbstractMigration
+final class Version20201027152657 extends AbstractMigration
 {
-    public function getDescription(): string
+    public function getDescription() : string
     {
         return '';
     }
 
-    public function up(Schema $schema): void
+    public function up(Schema $schema) : void
     {
-        $this->abortIf(
-            $this->connection
-                ->getDatabasePlatform()
-                ->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.'
-        );
         // this up() migration is auto-generated, please modify it to your needs
 
         $this->addSql("DROP TABLE IF EXISTS `mail_tbl_mail`;");
@@ -119,10 +113,30 @@ final class Version20200918143923 extends AbstractMigration
         (3,'Immediat','1 second'),
         (4,'Toutes les semaines','6 days 23 hours 51 minutes');
         ");
+
+        $this->addSql('
+        INSERT INTO mail_tbl_mail_type (`mail_type_id`,`mail_type_lib`,`mail_type_objet`,`mail_type_expediteur`,
+        `mail_type_cc`,`mail_type_bcc`,`mail_type_body`) VALUES 
+        (1,"Type Defaut","Test",NULL,"","","<div>
+        <p>Bonjour {{user_prenom}} {{user_nom}},</p><p>- identifiant : {{user_login}}</p><p>- mot de passe : {{user_password}}</p>");
+        ');
+
+        $this->addSql('
+            INSERT INTO mail_tbl_vue (`vue_id`,`vue_lib`,`vue_sql_propel`,`vue_post_sql_raw`,`vue_sql_raw`) VALUES
+            (1,"Vue Defaut",NULL,"Default","Default");
+        ');
+        $this->addSql('
+        INSERT INTO mail_tbl_regle (`regle_lib`,`vue_id`,`mail_type_id`,`regle_heure`,`regle_frequence_id`,`regle_actif`,
+        `regle_date_envoi`,`regle_renvoi`) VALUES 
+        ("Regle defaut",1,1,"00:00:00",3,0,"2015-01-01 00:00:00",0)
+    ');
+
+
     }
 
-    public function down(Schema $schema): void
+    public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+
     }
 }
